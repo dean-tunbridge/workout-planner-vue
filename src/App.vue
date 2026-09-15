85bee86e4ce744da186e56a54ccb3256ad571fef
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-15 18:56:56 +1200

    create starting components and fill boilerplate for each

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
-<template>
-  <HelloWorld />
-</template>
+<template></template>
+
+<style scoped></style>
