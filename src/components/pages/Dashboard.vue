635d2dcbad5c810e10c9b99d300f98aa2be7d3c2
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-16 20:02:15 +1200

    wokr on type errors in dashboard

--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -1,7 +1,66 @@
+<script setup lang="ts">
+import { gymHealthFacts } from '../../utils'
+// import Grid from './Grid.vue'
+
+interface Props {
+  handleSelectWorkout: (index: number) => void
+  firstIncompleteWorkoutIndex: number
+}
+
+const props = defineProps<Props>({
+  handleSelectWorkout: Function,
+  firstIncompleteWorkoutIndex: Number,
+})
+
+// generate a random whole integer number between 0 and array length - 1
+const randomNumber = Math.floor(Math.random() * gymHealthFacts.length)
+const todaysFact = gymHealthFacts[randomNumber]
+</script>
+
 <template>
-  <div></div>
+  <section id="dashboard">
+    <div class="card tip-container">
+      <h2>Welcome Soldier</h2>
+      <div>
+        <p class="tip"><strong>Daily Tip</strong><br />{{ todaysFact }}</p>
+      </div>
+      <button
+        @click="
+          () =>
+            handleSelectWorkout(
+              firstIncompleteWorkoutIndex < 0 ? 0 : firstIncompleteWorkoutIndex,
+            )
+        ">
+        Start workout &rarr;
+      </button>
+    </div>
+    <Grid v-bind="props" />
+  </section>
 </template>
 
-<script setup lang="ts"></script>
+<style scoped>
+.tip-container,
+.tip-container div,
+#dashboard {
+  display: flex;
+}
 
-<style scoped></style>
+.tip-container,
+#dashboard {
+  flex-direction: column;
+}
+
+#dashboard {
+  gap: 2rem;
+}
+
+.tip-container {
+  gap: 0.5rem;
+}
+
+@media (min-width: 640px) {
+  .tip-container {
+    gap: 1rem;
+  }
+}
+</style>
