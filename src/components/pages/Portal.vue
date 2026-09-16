ec4710b4d915931ad0d7ed47f314c0ebb21d97f8
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-17 08:39:42 +1200

    add teleport tag and begin filing content

--- src/components/pages/Portal.vue
+++ src/components/pages/Portal.vue
@@ -1,6 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Teleport } from 'vue'
+</script>
 <template>
-  <div></div>
+  <Teleport to="#portal">
+    <div class="portal-container">
+      <div
+        role="button"
+        tabindex="0"
+        @click="() => {}"
+        class="portal-underlay"></div>
+    </div>
+  </Teleport>
 </template>
 
 <style scoped></style>
