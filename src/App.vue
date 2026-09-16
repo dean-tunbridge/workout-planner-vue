3798b01de499c537416542c0a6eb9fb796efc6e2
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-16 19:47:48 +1200

    begin adding conditional rendering for each component in app

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ import Welcome from './components/pages/Welcome.vue'
 import Layout from './components/layouts/Layout.vue'
 import Dashboard from './components/pages/Dashboard.vue'
 import Workout from './components/pages/Workout.vue'
+
+const currDisplay = 1
 </script>
 
 <template>
   <Layout>
-    <Welcome />
-    <Dashboard />
-    <Workout />
+    <Welcome v-if="currDisplay == 0" />
+    <Dashboard v-if="currDisplay == 1" />
+    <Workout v-if="currDisplay == 2" />
   </Layout>
 </template>
 
