139cdc4fef65b7c0d9540c1b51e4b53a048b8706
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-17 08:31:58 +1200

    Add styles

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -61,4 +61,101 @@ const { workout, warmup } = workoutProgram[currWorkout]
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+#workout-card,
+.plan-card {
+  display: flex;
+  flex-direction: column;
+}
+
+#workout-card {
+  gap: 1.5rem;
+}
+
+.plan-card-header,
+.workout-btns {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+}
+
+.workout-grid,
+.workout-grid-row {
+  display: grid;
+  grid-template-columns: repeat(7, minmax(0, 1fr));
+  gap: 1rem;
+}
+
+.workout-grid-row,
+.workout-grid-line {
+  grid-column: span 7 / span 7;
+}
+
+.workout-grid-line {
+  margin: 0.5rem 0;
+  height: 3px;
+  border-radius: 2px;
+  background: var(--background-muted);
+}
+
+.grid-name {
+  grid-column: span 3 / span 3;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.grid-name button {
+  padding: 0;
+  border: none;
+  box-shadow: none;
+}
+
+.grid-name button:hover {
+  transform: none;
+  box-shadow: none;
+  color: var(--color-link);
+}
+
+.workout-grid-row .grid-name button {
+  opacity: 0;
+  pointer-events: none;
+}
+
+.workout-grid-row:hover .grid-name button {
+  opacity: 1;
+  pointer-events: all;
+}
+
+.grid-name p {
+  text-transform: capitalize;
+}
+
+.grid-weights {
+  grid-column: span 2 / span 2;
+}
+
+.workout-btns button {
+  flex: 1;
+}
+
+.workout-btns button i {
+  padding-left: 0.5rem;
+}
+
+.exercise-description {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+}
+
+.exercise-description h3 {
+  text-transform: capitalize;
+}
+
+.exercise-description button i {
+  padding-left: 0.5rem;
+}
+</style>
