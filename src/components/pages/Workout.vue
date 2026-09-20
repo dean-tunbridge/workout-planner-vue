2a8660e3f97b1e48bb0677c96d6e73ddb0b19cf3
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-21 11:38:31 +1200

    disable complete button depending on conditions

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -110,7 +110,7 @@ function handleCloseModal() {
       <button @click="handleSaveWorkout">
         Save & Exit <i class="fa-solid fa-save"></i>
       </button>
-      <button @click="handleSaveWorkout">
+      <button :disabled="isWorkoutComplete" @click="handleSaveWorkout">
         Complete <i class="fa-solid fa-check"></i>
       </button>
     </div>
