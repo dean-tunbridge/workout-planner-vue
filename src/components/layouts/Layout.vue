d093d63fd0a0bfb39e67aada66802640f464ca40
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-09-21 13:58:10 +1200

    add link to my github

--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -9,7 +9,7 @@
   </main>
   <footer>
     <small>Created by Dean</small>
-    <a href="#" target="blank">
+    <a href="https://github.com/dean-tunbridge" target="blank">
       <img
         alt="profile pic"
         src="https://images.unsplash.com/photo-1774021804386-daa60d7a6043?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
